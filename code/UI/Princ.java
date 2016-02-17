755e73b3df3e26763f4406829dd632891279e936
kreenox <jeangatien.cordier@gmail.com>
2016-02-17 10:44:36 +0100

    création de la première fenetre du programme

---
+++ code/UI/Princ.java
@@ -0,0 +1,17 @@
+
+import javax.swing.*;
+
+class Princ extends JFrame
+{
+
+	private JButton entre;//permet de gerer les entreprise
+	private JButton consult;//permet de voir les offres
+	private JButton anule;//quitte l'application
+	private JPanel;
+	//private JTextField VOID; juste pou plus tard aucun interet
+	public Princ()
+	{
+		
+	}
+
+}
