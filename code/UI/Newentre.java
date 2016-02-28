9885ae0b404b913e9c347138da738baa4e77062e
kreenox <jeangatien.cordier@gmail.com>
2016-02-28 20:50:58 +0100

    ajoute de la fenetre des nouvelles entreprises

---
+++ code/UI/Newentre.java
@@ -0,0 +1,20 @@
+
+import javax.swing.*;
+
+package UI;
+
+class newentre extends JFrame
+{
+	private JButton val = new JButton("valider");
+	private Jbutton anule = new JButton("annuler");
+	private JPanel pane;
+	private Jlabel = new JLabel("Veuillez entrer les donnés de l'entreprise");
+
+	public Newentre()
+	{
+		this.setTitle("GestStage - Nouvelle entreprise");
+		this set size(640, 480);
+
+	}
+
+}
