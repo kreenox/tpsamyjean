5d383e68079a2e4a1514c5bac80137a1197936db
kreenox <jeangatien.cordier@gmail.com>
2016-03-04 19:05:01 +0100

    juste un commit

--- code/control/Contr.java
+++ code/control/Contr.java
@@ -6,5 +6,17 @@ import javax.swing.*;
 
 public class Contr implements ActionListener{
 
+	//mettre toutes les fenetres ici
+	
+
+	public Contr(/*ajouter toute les fenetres ici*/)
+	{
+
+	}
+
+	public void ActionPerformed(ActionEvent e)
+	{
+		//on gere tout les boutons ici
+	}
 
 }
