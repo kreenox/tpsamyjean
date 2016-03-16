522879e88ec9fad9fa22ccd9b41bf8e3e803f054
VladTepes91 <greatsamouraikenshi@gmail.com>
2016-03-16 09:40:20 +0100

    mise à jour - ajoout actionlistener et actioncomand

--- code/UI/Princ.java
+++ code/UI/Princ.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class Princ extends JFrame
 {
-
+	
 	private JButton entre = new JButton("Gestion entreprise");//permet de gerer les entreprise
 	private JButton consult = new JButton("consulter\nles offres de stage");//permet de voir les offres
 	private JButton anule = new JButton("quitter");//quitte l'application
@@ -12,8 +12,9 @@ public class Princ extends JFrame
 	private JLabel text = new JLabel("Gestion des offres de stage\nUniversite d'Evry Val d'Essone");
 	private JTextField entrepriseUser = new JTextField("Identification siret", 14); //si il veut creer une nouvelle entreprise : mettre un 0
 	private JLabel conseil = new JLabel("Pour créer une nouvelle entreprise : mettez 0");
-	public Princ()
+	public Princ(ActionListener a)
 	{
+		
 		this.setTitle("GestStage - menu principal");
 		this.setSize(640, 480);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//si on veut pas utiliser l'application on arrete tout
@@ -47,6 +48,12 @@ public class Princ extends JFrame
 		pane.add(text);
 		pane.add(horizon);
 		pane.add(anule);
+		entre.addActionListener(a);
+		entre.setActionCommand("entre - princ");
+		consult.addActionListener(a);
+		consult.setActionCommand("consult - princ");
+		anule.addActionListener(a);
+		anule.setActionCommand("anule - princ");
 		//affichage
 		this.setVisible(true);
 	}
