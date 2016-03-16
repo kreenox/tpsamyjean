386a70c51b37e8d1ec5c0ebc744cbdb19627893e
kreenox <jeangatien.cordier@gmail.com>
2016-03-16 10:08:16 +0100

    ajout des actions pour la fenetre principales

--- code/control/Contr.java
+++ code/control/Contr.java
@@ -6,17 +6,57 @@ import javax.swing.*;
 
 public class Contr implements ActionListener{
 
-	//mettre toutes les fenetres ici
+	Princ		fenprinc;
+	ConsultStage	fenconsult;
+	Newstage	fennouvstage;
+	NewEntre	fennouventre;
 	
 
-	public Contr(/*ajouter toute les fenetres ici*/)
+	public Contr(Princ fen, ConsultStage cnsstg, NewStage nwstg, NewEntre nwntr)
 	{
-
+		fenprinc = fen;
+		fenconsult = cnsstg;
+		fennouvstage = nwstg;
+		fennouventre = nwntr;
 	}
 
-	public void ActionPerformed(ActionEvent e)
+	public void actionPerformed(ActionEvent e)
 	{
-		//on gere tout les boutons ici
+		switch(e.getActionCommand())
+		{
+		//newentre button
+		case "valide - newentre":
+			break;
+		case "annule - newentre":
+			break;
+		//newstage
+		case "val - newstage":
+			break;
+		case "annule - newstage":
+			break;
+		//consultstage
+		case "retour":
+			break;
+		//princ
+		case "entre - princ":
+			fenprinc.setVisible(false);
+			if(fenprinc.getEntrepriseUser() == "0";
+				fennouventre.setVisible(true);
+			else fennouvstage.setvisible(true);
+
+			break;
+		case "consult - princ":
+			fenprinc.setVisible(false);
+			fenconsult.setvisible(true);
+			break;
+		case "anule - princ":
+			System.exit(0);
+			break;
+		default:
+			break;
+
+		}
 	}
+
 
 }
