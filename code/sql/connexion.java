8dca2f7e4e1cf24b07e5f8c2e079f746be2ab24f
andre2505 <thomasdrege1@gmail.com>
2016-04-06 11:30:43 +0200

    correction sql

--- code/sql/connexion.java
+++ code/sql/connexion.java
@@ -1,10 +1,12 @@
 package sql;
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
-public class connexion {
+public static class connexion {
 
 
 	  private static Connection connect;
